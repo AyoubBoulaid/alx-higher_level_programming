370cae6136a5cbed65c1ae2762276ecac1a1bdab
Ayoub <boulaidayoub02@gmail.com>
2024-03-12 10:45:32 -0700

    Python Conditions

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
-"""I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game"""
 for letter in range (97, 123):
     print(f"{chr(letter)}", end="")
