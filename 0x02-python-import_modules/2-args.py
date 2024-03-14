3e16f18432a83a98d2d533fb6aee795233e82396
Ayoub <boulaidayoub02@gmail.com>
2024-03-14 12:03:44 -0700

    Import modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    count = len(sys.argv) - 1
+    
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i, arg in enumerate(sys.argv[1:], start=1):
+        print("{}: {}".format(i, arg))
