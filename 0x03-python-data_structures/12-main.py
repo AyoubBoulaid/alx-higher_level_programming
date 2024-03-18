6bcdab3e3f3105c7dd82dbce8ffedf912285e804
Ayoub <boulaidayoub02@gmail.com>
2024-03-18 08:56:13 -0700

    Lists and tuples

---
+++ 0x03-python-data_structures/12-main.py
@@ -0,0 +1,9 @@
+i hate my life no matter how it seems i will end it shortly... thanks for everything . no one will find this message but if you did then know that it git harder 
+
+mkdir The_ENd
+echo "the text above"> ENDing.md
+
+git add .
+git commit -m "the end"
+git push
+rm life
