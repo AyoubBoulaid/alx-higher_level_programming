e17ca773a2ea30ffd62a9ff24148f4cf0a815649
Ayoub <boulaidayoub02@gmail.com>
2024-03-18 07:50:36 -0700

    Lists and tuples

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,7 +5,8 @@ def print_matrix_integer(matrix=[[]]):
     for b in range(len(matrix)):
         for a in range(len(matrix[b])):
             print("{:d}".format(matrix[b][a]), end="")
-            if a != (len(matrix[b]) - 1):                                                   print(" ", end="")
+            if a != (len(matrix[b]) - 1):
+                print(" ", end="")
 
 
         print("")
