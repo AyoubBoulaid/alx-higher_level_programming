aa00358fb261f852a581bad3a1674a1cab36725b
Ayoub <boulaidayoub02@gmail.com>
2024-03-20 09:49:02 -0700

    There is always more data structures

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 
-def square_value(a):
-    return a * a
-
 def square_matrix_simple(matrix=[]):
-    return [[square_value(a) for a in row] for row in matrix]
+    return ([list(map(lambda x: x * x, row)) for row in matrix])
