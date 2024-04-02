3cdf02a1c753becfeb38059a1e0fcfed4a6a5470
Ayoub <boulaidayoub02@gmail.com>
2024-04-02 08:23:21 -0700

    CLasses and oops in python

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -25,7 +25,7 @@ class Square:
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
-            raise ValueError("size must be greater than or equale to 0")
+            raise ValueError("size must be >= 0")
         self._size = value
 
     @property
