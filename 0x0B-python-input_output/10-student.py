aece19b6f14c44b393f7ba9f8166ee4670dee687
Ayoub <boulaidayoub02@gmail.com>
2024-04-16 10:21:33 -0700

    In/Out put in python

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -10,7 +10,7 @@ class Student:
         first_name (str): first name of student.
         last_name (int): last name of student.
         age (int): age of student.
-        """
+    """
 
     def __init__(self, first_name, last_name, age):
         """Initialize a new Student.
@@ -24,10 +24,16 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """Retrieves a dictionary representation of a Student instance.
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the Student.
 
-        Returns:
-            dict: dictionary representation.
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
+
+        Args:
+            attrs (list): (Optional) The attributes to represent.
         """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
