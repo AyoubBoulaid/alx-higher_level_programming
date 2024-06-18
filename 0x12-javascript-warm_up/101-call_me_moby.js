4a931165448570feed6026c66d3e4429df9f4eda
AYoub <boulaidayoub02@gmail.com>
2024-06-18 13:39:05 -0700

    JS is fun but only as a warm up

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,4 +1,6 @@
-g!/usr/bin/node
+#!/usr/bin/node
 exports.callMeMoby = function (x, theFunction) {
-  for (let i = 0; i < x; i++) theFunction();
+  for (let i = 0; i < x; i++) {
+	  theFunction();
+  }
 };
