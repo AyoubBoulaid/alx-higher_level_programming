6bd9336516056e1b0c598a9aeedd37a4d182f1e6
AyoubBoulaid <boulaidayoub02@gmail.com>
2024-06-19 13:03:49 -0700

    Tasks in JS around Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,10 +2,11 @@
 const dict = require('./101-data.js').dict;
 let newDict = {};
 for (let key in dict) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [key];
+  let value = dict[key];
+  if (newDict[value] === undefined) {
+     newDict[value] = [key];
   } else {
-    newDict[dict[key]].push(key);
+    newDict[value].push(key);
   }
 }
 console.log(newDict);
