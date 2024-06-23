9caafff514c44717046ee4518dbae8581dc52fc2
Ayoub <boulaidayoub02@gmail.com>
2024-06-23 13:57:03 -0700

    MySQL && Python ORMapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,13 +7,7 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    
-    results = cursor.fetchall()
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall()]
