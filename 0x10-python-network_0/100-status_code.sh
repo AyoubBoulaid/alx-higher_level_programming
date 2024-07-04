44587aada2656e26e014dbd6cdb13ed35485262d
Ayoub Boulaid <boulaidayoub02@gmail.com>
2024-07-04 05:07:11 -0700

    Networking with Python #0

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # bash script to display status code of server
 
-awk 'NR==1{printf "%s", $2}' test7 $(curl -sI "$1" -o test7)
+curl -so /dev/null -w '%{http_code}' "$1"
--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # script that was a fun effort in breaking to http protocols on ALX servers
 
-curl -o /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me
+curl -sLX PUT -d "user_id=98" -H "Origin:HolbertonSchool" 0.0.0.0:5000/catch_me
