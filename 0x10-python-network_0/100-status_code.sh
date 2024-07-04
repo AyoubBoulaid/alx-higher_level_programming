8f569f4c91abd385c3d9d9eafedd89fc8e3d6a15
Ayoub Boulaid <boulaidayoub02@gmail.com>
2024-07-04 04:58:31 -0700

    Networking with Python #0

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 # bash script to display status code of server
 
-if [ -z "$1" ]; then
-	echo "usage: $0 <curl>"
-	exit 1
-fi
-
-curl -s -o /dev/null -w "%{http_code}" "$1"
+curl -o /dev/null -sw "%{http_code}" $1
