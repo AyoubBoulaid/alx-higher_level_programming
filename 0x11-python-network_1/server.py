3de3d5d7513d3faa815e6f93176ce68d27121021
Ayoub Boulaid <boulaidayoub02@gmail.com>
2024-07-05 07:02:39 -0700

    Python --Network#1

---
+++ 0x11-python-network_1/server.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Index"
+
+@app.route('/status_401')
+def status_401():
+    return "Unauthorized", 401
+
+@app.route('/doesnt_exist')
+def doesnt_exist():
+    return "Not Found", 404
+
+@app.route('/status_500')
+def status_500():
+    return "Internal Server Error", 500
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
