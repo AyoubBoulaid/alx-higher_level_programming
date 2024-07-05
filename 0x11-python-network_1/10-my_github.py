bec35473faa68b74ce5154adb068e62e07ee14dc
Ayoub Boulaid <boulaidayoub02@gmail.com>
2024-07-05 07:11:12 -0700

    Python --Network#1

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,22 +1,12 @@
 #!/usr/bin/python3
 """script for posting data to star wars api
 """
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+
 if __name__ == "__main__":
-    import requests
-    from requests.auth import HTTPBasicAuth
-    import sys
-    url = "https://api.github.com/"
-    user_url = url + "user"
-    username = sys.argv[1]
-    password = sys.argv[2]
-    response = requests.get(user_url,
-                            auth=HTTPBasicAuth(username,
-                                               password))
-    if response.status_code == requests.codes.ok and len(response.text) > 0:
-        try:
-            my_obj = response.json()
-            print(user_info['id'])
-        except ValueError as invalid_json:
-            print('Not a valid JSON')
-    else:
-        print(None)
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
